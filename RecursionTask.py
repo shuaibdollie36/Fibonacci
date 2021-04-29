872492f2940691269fcd2e829768142d58285a0e
Shuaibdollie36 <shuaibdollie36@gmail.com>
2021-04-29 11:51:17 +0200

    modified file

--- RecursionTask.py
+++ RecursionTask.py
@@ -1,21 +1,12 @@
-terms = int(input("How many terms: "))
-#Terms
-b1, b2 = 0, 1
-count = 0
-#check if valid
-if terms <= 0:
-  print ("Enter a postive integer")
-elif terms == 1:
-  print ("Sequence Fibonacci",terms,":")
-  print(b1)
-else:
-  print ("Sequence Fibonacci:")
-  while count < terms:
-      print(b1, end=" ")
-      nth = b1 + b2
-      b1 = b2
-      b2 = nth
-      count += 1
+
+#Define a function using recursion that returns first 20
+def fib(a):
+    if a <= 1:
+        return a
+    else:
+        return fib(a - 1) + fib(a - 2)
 
 
-
+#Display Value
+for i in range(20):
+    print(str(fib(i)), end=" ")
